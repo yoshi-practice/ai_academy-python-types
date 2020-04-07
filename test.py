51c7c09f6c15ef2c8ac36c62a7ee8219864c73b3
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-04-07 22:02:29 +0900

    Add test answer

---
+++ test.py
@@ -0,0 +1,8 @@
+years_list = [2017, 2018, 2019, 2020, 2021]
+print(years_list[3])
+print(years_list[4])
+
+fav_word = ["Apple", "banana", "choco"]
+fav_word.append("candy")
+
+user_data = {"田中": 35, "山田": 42, "鈴木": 28}
