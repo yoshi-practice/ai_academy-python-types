64ed0701746a400c856037fed63b504e367c7fbe
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-04-07 21:52:10 +0900

    Add strings

---
+++ strings.py
@@ -0,0 +1,16 @@
+str1 = 'Single quote'
+str2 = "Double quote"
+str3 = """Three
+quote"""
+
+# ---
+
+a = "Hello"
+b = "world"
+print(a + b)  # Hello world
+
+# ---
+
+a = "Hello"
+print(a * 3)  # HelloHelloHello
+
