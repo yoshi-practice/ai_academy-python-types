cee4b893ea98d99d407ac09fdbf0f30286a9b18d
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-04-07 21:52:53 +0900

    Add sample script

---
+++ cast.py
@@ -0,0 +1,8 @@
+print("Hello" + "World")  # +により文字列同士を連結
+name = "Tom"
+print("My name is " + name)
+
+age = 24
+# 以下は、データ型の異なる文字列型と数値型を連結してエラーになっているプログラムです。
+
+print("My name is " + name + "My age is " + age)  # この行はエラー
